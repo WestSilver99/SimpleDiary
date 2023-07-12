88d030482e08ade0b87090a2e445d04acf52ad4f
Seo Eun <seo606101@naver.com>
2023-07-12 22:27:34 +0900

    feat : 버튼까지 완성

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,26 +1,56 @@
 import { useState } from "react";
 
 const DiaryEditor = () => {
-  const [author, setAuthor] = useState("");
-  const [content, setContent] = useState("");
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1,
+  });
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = () => {
+    console.log(state);
+    alert("저장 성공");
+  };
+
   return (
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
         <input
-          onChange={(e) => {
-            setAuthor(e.target.value);
-          }}
-          value={author}
+          name="author"
+          onChange={handleChangeState}
+          value={state.author}
         />
       </div>
       <div>
         <textarea
-          value={content}
-          onChange={(e) => {
-            setContent(e.target.value);
-          }}
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
         />
+      </div>
+      <div>
+        오늘의 감정점수 :
+        <select
+          name="emtion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
       </div>
     </div>
   );
