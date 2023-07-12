bbf36d9b726664d71582f03ccacf68d8eadd2796
Seo Eun <seo606101@naver.com>
2023-07-12 22:02:07 +0900

    feat : input, textarea 생성

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,4 +1,28 @@
+import { useState } from "react";
+
 const DiaryEditor = () => {
-  return <div className="DiaryEditor">hi</div>;
+  const [author, setAuthor] = useState("");
+  const [content, setContent] = useState("");
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
+          value={author}
+        />
+      </div>
+      <div>
+        <textarea
+          value={content}
+          onChange={(e) => {
+            setContent(e.target.value);
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 export default DiaryEditor;
